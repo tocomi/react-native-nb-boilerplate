ffd97e5a075e1d0e42db0af75c7d8099117cc5d8
tocomi <tocomi0112@gmail.com>
2022-01-19 01:20:33 +0900

    style: apply linter

--- App.tsx
+++ App.tsx
@@ -1,39 +1,38 @@
-import 'expo-dev-client';
-import React, {useCallback, useEffect, useState} from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-import {LogBox} from 'react-native';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import 'expo-dev-client'
+import * as SplashScreen from 'expo-splash-screen'
+import React, { useCallback, useEffect, useState } from 'react'
+import { LogBox } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { AppNavigator } from './src/app'
+import { initServices } from './src/services'
+import { hydrateStores, StoresProvider } from './src/stores'
+import { configureDesignSystem } from './src/utils/designSystem'
 
-import {AppNavigator} from './src/app';
-import {configureDesignSystem} from './src/utils/designSystem';
-import {hydrateStores, StoresProvider} from './src/stores';
-import {initServices} from './src/services';
-
-LogBox.ignoreLogs(['Require']);
+LogBox.ignoreLogs(['Require'])
 
 export default (): JSX.Element => {
-  const [ready, setReady] = useState(false);
+  const [ready, setReady] = useState(false)
 
   const startApp = useCallback(async () => {
-    await SplashScreen.preventAutoHideAsync();
+    await SplashScreen.preventAutoHideAsync()
 
-    await hydrateStores();
-    await initServices();
-    configureDesignSystem();
+    await hydrateStores()
+    await initServices()
+    configureDesignSystem()
 
-    setReady(true);
-    await SplashScreen.hideAsync();
-  }, []);
+    setReady(true)
+    await SplashScreen.hideAsync()
+  }, [])
 
   useEffect(() => {
-    startApp();
-  }, [startApp]);
+    startApp()
+  }, [startApp])
 
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StoresProvider>
         <StoresProvider>{ready ? <AppNavigator /> : null}</StoresProvider>
       </StoresProvider>
     </GestureHandlerRootView>
-  );
-};
+  )
+}
